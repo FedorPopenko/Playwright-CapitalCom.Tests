5185f20f11643bc5421cdf0f897cd9626337e469
Fedor Popenko <popenko.fedor@gmail.com>
2026-09-07 17:41:04 +0400

    Add Press Centre page and tests

---
+++ CapitalCom.Tests/Pages/About/Who_We_Are/PressCentrePage.cs
@@ -0,0 +1,35 @@
+﻿using CapitalCom.Tests.Core.Models;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace CapitalCom.Tests.Pages.About.Who_We_Are
+{
+    public class PressCentrePage
+    {
+        private readonly IPage _page;
+
+        private ILocator BannerOnThePage => _page.Locator("p:has-text('shamillia.sivathambu@capital.com')");
+        private ILocator ButtonTrade => _page.Locator("button[data-type='wdg_most_traded_btn']").First;
+
+        public PressCentrePage(IPage page)
+        {
+            _page = page;
+            loginAndSignUpForm = new LoginAndSignUpForm(page);
+            locationForm = new LocationForm(page);
+        }
+
+        public LoginAndSignUpForm loginAndSignUpForm { get; }
+        public LocationForm locationForm { get; }
+
+        public async Task ExpectLoadedPressCentrePageAsync()
+        {
+            await Assertions.Expect(_page).ToHaveURLAsync(new Regex(CapitalPagePath.PressCentrePage));
+            await Assertions.Expect(BannerOnThePage).ToBeVisibleAsync();
+        }
+
+        public async Task ClickTradeButtonAsync()
+        {
+            await ButtonTrade.ClickAsync();
+        }
+    }
+}
---
+++ CapitalCom.Tests/Tests/PressCentreTests.cs
@@ -0,0 +1,42 @@
+﻿using CapitalCom.Tests.Core;
+using CapitalCom.Tests.Core.Artifacts;
+using CapitalCom.Tests.Core.Models;
+using CapitalCom.Tests.Pages.About.Who_We_Are;
+
+namespace CapitalCom.Tests;
+
+[Parallelizable(ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class PressCentreTests : CapitalTestBase
+{
+    [SetUpFixture]
+    public sealed class TestRunCleanup
+    {
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            ArtifactClaener.DeletePassedVideos();
+        }
+    }
+
+    [TestCaseSource(typeof(TestMatrix), nameof(TestMatrix.EnglishContexts))]
+    public async Task PressCentrePage_ShouldOpenSuccessfully(TestRunContext context)
+    {
+        await OpenCapitalPageAsync(context, CapitalPagePath.PressCentrePage);
+        var pressCentrePage = new PressCentrePage(Page);
+
+        await pressCentrePage.ExpectLoadedPressCentrePageAsync();
+    }
+
+    [TestCaseSource(typeof(TestMatrix), nameof(TestMatrix.EnglishContexts))]
+    public async Task TradeButton_ShouldOpenExpectedDestination(TestRunContext context)
+    {
+        await OpenCapitalPageAsync(context, CapitalPagePath.PressCentrePage);
+        var pressCentrePage = new PressCentrePage(Page);
+
+        await CloseLocationFormIfDisplayedAsync(pressCentrePage.locationForm);
+
+        await ExpectСtaClickResultAsync(context, () => pressCentrePage.ClickTradeButtonAsync(),
+            pressCentrePage.loginAndSignUpForm);
+    }
+}
