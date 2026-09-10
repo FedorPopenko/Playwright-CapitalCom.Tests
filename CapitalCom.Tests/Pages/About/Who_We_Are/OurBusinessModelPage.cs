603955a554385c62aadc46604b56e74e54496f6c
Fedor Popenko <popenko.fedor@gmail.com>
2026-09-10 12:55:05 +0400

    Add Our Business page and tests

---
+++ CapitalCom.Tests/Pages/About/Who_We_Are/OurBusinessModelPage.cs
@@ -0,0 +1,35 @@
+﻿using CapitalCom.Tests.Core.Models;
+using Microsoft.Playwright;
+using System.Text.RegularExpressions;
+
+namespace CapitalCom.Tests.Pages.About.Who_We_Are
+{
+    public class OurBusinessModelPage
+    {
+        private readonly IPage _page;
+
+        private ILocator SpreadsBlock => _page.Locator("p:has-text('EUR/USD')");
+        private ILocator ButtonCreateYourAccount => _page.Locator("button[data-type='banner_with_steps']");
+
+        public OurBusinessModelPage(IPage page)
+        {
+            _page = page;
+            loginAndSignUpForm = new LoginAndSignUpForm(page);
+            locationForm = new LocationForm(page);
+        }
+
+        public LoginAndSignUpForm loginAndSignUpForm { get; }
+        public LocationForm locationForm { get; }
+
+        public async Task ExpectLoadedOurBusinessModelPageAsync()
+        {
+            await Assertions.Expect(_page).ToHaveURLAsync(new Regex(CapitalPagePath.OurBusinessModelPage));
+            await Assertions.Expect(SpreadsBlock).ToBeVisibleAsync();
+        }
+
+        public async Task ClickCreateYourAccountButtonAsync()
+        {
+            await ButtonCreateYourAccount.ClickAsync();
+        }
+    }
+}
---
+++ CapitalCom.Tests/Tests/OurBusinessModelTests.cs
@@ -0,0 +1,42 @@
+﻿using CapitalCom.Tests.Core;
+using CapitalCom.Tests.Core.Artifacts;
+using CapitalCom.Tests.Core.Models;
+using CapitalCom.Tests.Pages.About.Who_We_Are;
+
+namespace CapitalCom.Tests;
+
+[Parallelizable(ParallelScope.All)]
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class OurBusinessModelTests : CapitalTestBase
+{
+    [SetUpFixture]
+    public sealed class TestRunCleanup
+    {
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            ArtifactClaener.DeletePassedVideos();
+        }
+    }
+
+    [TestCaseSource(typeof(TestMatrix), nameof(TestMatrix.SmokeContexts))]
+    public async Task OurBusinessModelPage_ShouldOpenSuccessfully(TestRunContext context)
+    {
+        await OpenCapitalPageAsync(context, CapitalPagePath.OurBusinessModelPage);
+        var ourBusinessModelPage = new OurBusinessModelPage(Page);
+
+        await ourBusinessModelPage.ExpectLoadedOurBusinessModelPageAsync();
+    }
+
+    [TestCaseSource(typeof(TestMatrix), nameof(TestMatrix.SmokeContexts))]
+    public async Task CreateYourAccountButton_ShouldOpenExpectedDestination(TestRunContext context)
+    {
+        await OpenCapitalPageAsync(context, CapitalPagePath.OurBusinessModelPage);
+        var ourBusinessModelPage = new OurBusinessModelPage(Page);
+
+        await CloseLocationFormIfDisplayedAsync(ourBusinessModelPage.locationForm);
+
+        await ExpectСtaClickResultAsync(context, () => ourBusinessModelPage.ClickCreateYourAccountButtonAsync(),
+            ourBusinessModelPage.loginAndSignUpForm);
+    }
+}
